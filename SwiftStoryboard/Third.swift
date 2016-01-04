0e6e2f6f5de86cd8ad866085fb6efdeb441264ac
benben <benben>
2016-01-03 20:44:28 -0600

    first

---
+++ SwiftStoryboard/Third.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SwiftStoryboard
+//
+//  Created by benjaminhallock@gmail.com on 1/3/16.
+//  Copyright © 2016 ben. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
